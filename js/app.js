ab369da60304331e67619743042303a5343f47c7
Thomas Tilahun <thomm.sef@gmail.com>
2020-02-27 16:42:50 -0800

    lab 02 final

--- js/app.js
+++ js/app.js
@@ -10,47 +10,22 @@ function Image (imgObject) {
   this.horns = imgObject.horns;
 }
 
-
-let keyword = [];
-
-Image.prototype.filterImage = function () {
-    imgArr.forEach(value => {
-   if(keyword.indexOf(value.keyword) < -1) {
-    } else {
-        keyword.push(imgArr.value);
-    
-};
-    });
-// keyword.forEach(value => ($(`select`).append(`<option id ="option_${value}">${value}</option>`));
-
-     
-
-    
-
-};
-
+// render function
 Image.prototype.render = function() {
-    $('main').append('<div class="clone"></div>');
-    let imageClone = $('div[class="clone"]');
-
-    let imageHtml = $('#photo-template').html();
-
-    imageClone.html(imageHtml);
-
-    imageClone.find('h2').text(this.title);
-    imageClone.find('img').attr('src', this.image_url); 
-    imageClone.find('img').attr('alt', this.title);    
-    imageClone.find('p').text(this.description);
-    imageClone.removeClass('clone');
-    imageClone.attr('class' , this.keyword);
-    imageClone.addClass('tomObject');
-
-    
+  $('main').append('<div class="clone"></div>');
+  let imageClone = $('div[class="clone"]');
+  let imageHtml = $('#photo-template').html();
+  imageClone.html(imageHtml);
+  imageClone.find('h2').text(this.title);
+  imageClone.find('img').attr('src', this.image_url); imageClone.find('img').attr('alt', this.title);
+  imageClone.find('p').text(this.description);
+  imageClone.removeClass('clone');
+  imageClone.attr('class' , this.keyword);
 };
-       
-    Image.readJson = () => { 
-    $.get('../data/page-1.json','json')
 
+//generate images from page-1
+Image.readJson = () => {
+  $.ajax('../data/page-1.json','json')
     .then(imgData => {
       imgData.forEach(imageItem => {
         imgArr.push(new Image(imageItem));
@@ -64,51 +39,40 @@ Image.loadImage = () => {
   renderList();
 };
 
-
-
-
-
-
-Image.prototype.renderList = function() {
-    //selecting the parent & creating an option
-    $('select').append('<option class="clone">Filter By Key Word</option>');
-    
-    $('option').append('<p></p>');
-    
-    
-    let listClone = $('option[class="clone"]');
-    //fill the option
-    listClone.find('option').text(this.keyword)
-    listClone.removeClass('clone');
-    listClone.attr('class', this.keyword);
-    
-    
-    //fill the option
-    listClone.find('p').text(this.title)
-    
-    $('select').on('change',(e)=> {
-        let option = e.target.value;
-        $('div').hide();
-        $(`.${option}`).show();
-        // console.log('select');
-    });
-};
-
 $(() => Image.readJson());
 
-// Image.imagesFilter = () => {
+//generate keyword list
+let keywordFilter = [];
 
-// let filterkeyword = [];
-// $('option').not(':first').remove();
-// Image.imgArr.forEach(display =>{
-//     if(!filterkeyword.includes(imgObject.keyword)) 
-//     filterkeyword.push(imgObject.keyword);
-// });
-//     filterkeyword.sort();
-//     filterkeyword.forEach(keyword => {
-//       let optionTag = `<option value="${keyword}">${keyword}</option>`;
-//       $('select').append(optionTag);
-//     });
-// };
+function renderList () {
+  imgArr.forEach((keywordCheck) => {
+    if(!keywordFilter.includes(keywordCheck.keyword)) {
+      console.log(keywordCheck.keyword);
+      keywordFilter.push(keywordCheck.keyword);
+      $('select').append(`<option>${keywordCheck.keyword}</option>`);
+    }
+  });
+}
 
+Image.prototype.renderList = function() {
+  //select the parent & creating an option
+  imgArr.forEach();
+  $('option').append('<p></p>');
+  let $listClone = $('option[class="clone"]');
+  //fill the option
+  $listClone.find('option').text(this.keyword);
+  $listClone.removeClass('clone');
+  $listClone.attr('class', this.keyword);
+  //find the option
+  $listClone.find('p').text(this.keyword);
+};
 
+//filter by keyword
+keywordFilter.forEach(function(value) {
+  $('select') .append(`<option id="option_${value}">${value}</option>`);});
+
+$('select').on('change', (event) => {
+  let option = event.target.value;
+  $('div').hide();
+  $(`.${option}`).show();
+});
